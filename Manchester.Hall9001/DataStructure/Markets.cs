e897650663e9f956625b1f779a3468aad01e484e
Jaison Carvalho <jaisoncarvalho@hotmail.com>
2018-08-12 19:54:11 -0300

    .

---
+++ Manchester.Hall9001/DataStructure/Markets.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manchester.Hall9001
+{
+    public class Markets
+    {
+        public int idMarket { get; set; }
+        public string coinCodeFrom { get; set; }
+        public string coinCodeto { get; set; }
+        public string coinNameFrom { get; set; }
+        public string coinNameTo { get; set; }
+        public int decimalPlacesCoinFrom { get; set; }
+        public int decimalPlacesCoinTo { get; set; }
+        public string descriptionCoinRelation { get; set; }
+    }
+}
---
+++ Manchester.Hall9001/DataStructure/Ticker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manchester.Hall9001
+{
+    public class Ticker
+    {
+        public decimal buy { get; set; }
+        public decimal sell { get; set; }
+        public int timestamp { get; set; }
+        public string coinFrom { get; set; }
+        public string coinTo { get; set; }
+        public decimal min { get; set; }
+        public decimal max { get; set; }
+        public decimal last { get; set; }
+        public decimal volume { get; set; }
+    }
+}
